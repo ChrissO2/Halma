97e9b28aebc38c230875f0689b46ad9ac6d0965f
Krzysztof Otreba <krzysztof.otreba02@gmail.com>
2024-05-05 20:36:41 +0200

    added new heuristics

--- heuristics.py
+++ heuristics.py
@@ -10,7 +10,15 @@ class Heuristics:
 
     @staticmethod
     def dist_from_oponent_corner(board: Board, player: int):
-        pass
+        MAX_DIST = 510
+        MIN_DIST = 60
+        corner = (15, 15) if player == 1 else (0, 0)
+        result = 0
+        for row in range(16):
+            for col in range(16):
+                if board.board[row][col] == player:
+                    result += abs(row - corner[0]) + abs(col - corner[1])
+        return (result - MIN_DIST) / (MAX_DIST - MIN_DIST)
 
     @staticmethod
     def pawn_isolation(board: Board, player: int):
@@ -49,5 +57,29 @@ class Heuristics:
 
     @staticmethod
     def no_possible_moves(board: Board, player: int):
-        MAX_POSSIBLE_MOVES = 1500
-        return sum(len(value) for value in board.get_all_possible_moves().values()) / MAX_POSSIBLE_MOVES
+        MAX_POSSIBLE_MOVES = 400
+        result = sum(len(value) for value in board.get_all_possible_moves(
+        ).values()) / MAX_POSSIBLE_MOVES
+        return result if 0 <= result <= 1 else 1
+
+    @staticmethod
+    def no_pawns_on_edge(board: Board, player: int):
+        MAX_RESULT = 19
+        result = 0
+        for row in range(16):
+            for col in range(16):
+                if board.board[row][col] == player:
+                    if row == 0 or row == 15 or col == 0 or col == 15:
+                        result += 1
+        return result / MAX_RESULT
+
+    @staticmethod
+    def no_pawns_on_diagonal(board: Board, player: int):
+        MAX_RESULT = 19
+        result = 0
+        for row in range(16):
+            for col in range(16):
+                if board.board[row][col] == player:
+                    if row == col or row == 15 - col:
+                        result += 1
+        return result / MAX_RESULT
