7b34406c6d80204656b31105072186ba30f10588
Krzysztof Otreba <krzysztof.otreba02@gmail.com>
2024-05-05 20:37:02 +0200

    added function to test max number of possible moves

--- main.py
+++ main.py
@@ -1,105 +1,28 @@
-class Pawn:
-    def __init__(self, board, color: True) -> None:
-        self.color = color
-        self.board = board
-
-    def __str__(self) -> str:
-        return '1' if self.color else '0'
-
-    def get_possible_moves(self, came_from=[]):
-        directions = (
-            (1, 0),
-            (-1, 0),
-            (0, 1),
-            (0, -1),
-            (1, 1),
-            (-1, 1),
-            (1, -1),
-            (-1, -1),
-        )
-        possible_moves = []
-        for direction in directions:
-            if direction in came_from:
-                continue
-            new_row = self.row + direction[0]
-            new_col = self.col + direction[1]
-            if not (0 <= new_row < self.board.BOARD_SIZE and 0 <= new_col < self.board.BOARD_SIZE):
-                continue
-            if not self.board[new_row][new_col]:
-                continue
-            if not self.board[new_row][new_col]:
-                # move there and finish
-                pass
-            else:
-                # jump over the pawn and search for new moves
-                pass
+from game import Board
+import random
+from time import time
+from heuristics import Heuristics
 
 
-class Board:
-    def __init__(self, board=None, turn=True) -> None:
-        self.BOARD_SIZE = 16
-        if not board:
-            self.board = [[None for _ in range(16)] for _ in range(16)]
-            self.init_board()
-        else:
-            self.board = board.copy()
-        self.turn = turn
+def get_max_pos_moves():
+    b = Board()
+    no_pos_moves = []
+    for _ in range(10000):
+        pos_moves = b.get_all_possible_moves()
+        if not pos_moves:
+            raise ValueError('No possible moves')
 
-    def __str__(self) -> str:
-        return '\n'.join([' '.join([str(cell) for cell in row]) for row in self.board]) + '\n'
+        no_pos_moves.append(sum(len(value) for value in pos_moves.values()))
 
-    def print_board(self):
-        for row in self.board:
-            row_str = ''
-            for cell in row:
-                if cell:
-                    row_str += str(cell) + ' '
-                else:
-                    row_str += '- '
-            print(row_str.strip())
-        print('\n\n')
+        for (row, col), moves in pos_moves.items():
+            if len(moves) != len(set(moves)):
+                raise ValueError('Duplicate moves')
 
-    def add_pawn(self, row, col, color: True):
-        if 0 <= row < self.BOARD_SIZE and 0 <= col < self.BOARD_SIZE:
-            if not self.board[row][col]:
-                self.board[row][col] = Pawn(self, color)
-            else:
-                raise ValueError('Cell is already occupied')
-        else:
-            raise ValueError('Invalid position')
+        key = random.choice(list(pos_moves.keys()))
+        move = random.choice(pos_moves[key])
+        b.move_pawn(key[0], key[1], move[0], move[1])
 
-    def init_board(self):
-        player1_positions = (
-            (0, 0), (0, 1), (0, 2), (0, 3), (0, 4),
-            (1, 0), (1, 1), (1, 2), (1, 3), (1, 4),
-            (2, 0), (2, 1), (2, 2), (2, 3),
-            (3, 0), (3, 1), (3, 2),
-            (4, 0), (4, 1),
-        )
-        for pos in player1_positions:
-            self.add_pawn(pos[0], pos[1], True)
-            self.add_pawn(self.BOARD_SIZE - 1 -
-                          pos[0], self.BOARD_SIZE - 1 - pos[1], False)
-
-    def move_pawn(self, row, col, new_row, new_col):
-        if not all((0 <= row < self.BOARD_SIZE,
-                   0 <= col < self.BOARD_SIZE,
-                   0 <= new_row < self.BOARD_SIZE,
-                   0 <= new_col < self.BOARD_SIZE)):
-            raise ValueError('Cell out of range')
-        if not self.board[row][col]:
-            raise ValueError('No pawn in the cell')
-        if self.board[new_row][new_col]:
-            raise ValueError('New cell is already occupied')
-        if self.board[row][col].color != self.turn:
-            raise ValueError('Not your turn')
-
-        self.board[new_row][new_col] = self.board[row][col]
-        self.board[row][col] = '-'
-        self.turn = not self.turn
+    return max(no_pos_moves)
 
 
-b = Board()
-b.print_board()
-b.move_pawn(0, 4, 0, 5)
-b.print_board()
+print(Heuristics.dist_from_oponent_corner(Board(), 1))
