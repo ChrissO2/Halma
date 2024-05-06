c659e604a423a88e08914a29b9388d0d9e440ed3
Krzysztof Otreba <krzysztof.otreba02@gmail.com>
2024-05-06 21:50:48 +0200

    fixed minimax algorithm and deleted unused stuff

--- minimax.py
+++ minimax.py
@@ -17,6 +17,8 @@ def minimax(board: Board, depth: int, maximizing: bool, player: int, eval_func) 
         for pawn, move in board.get_all_possible_moves().items():
             # pawn (row, col), move [(new_row, new_col), ...]
             for new_row, new_col in move:
+                if not best_move:
+                    best_move = (pawn, (new_row, new_col))
                 new_board = Board(board.board, board.turn)
                 new_board.move_pawn(pawn[0], pawn[1], new_row, new_col)
                 eval = minimax(new_board, depth - 1,
@@ -31,6 +33,8 @@ def minimax(board: Board, depth: int, maximizing: bool, player: int, eval_func) 
         min_eval = float('inf')
         for pawn, move in board.get_all_possible_moves().items():
             for new_row, new_col in move:
+                if not best_move:
+                    best_move = (pawn, (new_row, new_col))
                 new_board = Board(board.board, board.turn)
                 new_board.move_pawn(pawn[0], pawn[1], new_row, new_col)
                 eval = minimax(new_board, depth - 1,
@@ -80,50 +84,6 @@ def alfabeta(board: Board, depth: int, maximizing: bool, player: int, eval_func,
                 new_board.move_pawn(pawn[0], pawn[1], new_row, new_col)
                 eval = alfabeta(new_board, depth - 1, True,
                                 player, eval_func, alfa, beta)[0]
-                if eval < min_eval:
-                    min_eval = eval
-                    best_move = (pawn, (new_row, new_col))
-                beta = min(beta, eval)
-                if beta <= alfa:
-                    break
-        return min_eval, best_move
-
-
-def alfabeta_known_positions(board: Board, depth: int, maximizing: bool, player: int, eval_func, alfa=float('-inf'), beta=float('+inf'), known_pos=[]) -> float:
-    is_over = board.is_game_over()
-    if is_over:
-        return (float('+inf'), None) if is_over == player else (float('-inf'), None)
-
-    if depth == 0:
-        return eval_func(board, player, known_pos), None
-
-    if maximizing:
-        best_move = None
-        max_eval = -float('inf')
-        for pawn, move in board.get_all_possible_moves().items():
-            # pawn (row, col), move [(new_row, new_col), ...]
-            for new_row, new_col in move:
-                new_board = Board(board.board, board.turn)
-                new_board.move_pawn(pawn[0], pawn[1], new_row, new_col)
-                eval = alfabeta_known_positions(new_board, depth - 1, False,
-                                                player, eval_func, alfa, beta, known_pos=known_pos)[0]
-                if eval > max_eval:
-                    max_eval = eval
-                    best_move = (pawn, (new_row, new_col))
-                alfa = max(alfa, eval)
-                if beta <= alfa:
-                    break
-        return max_eval, best_move
-
-    else:
-        best_move = None
-        min_eval = float('inf')
-        for pawn, move in board.get_all_possible_moves().items():
-            for new_row, new_col in move:
-                new_board = Board(board.board, board.turn)
-                new_board.move_pawn(pawn[0], pawn[1], new_row, new_col)
-                eval = alfabeta_known_positions(new_board, depth - 1, True,
-                                                player, eval_func, alfa, beta, known_pos=known_pos)[0]
                 if eval < min_eval:
                     min_eval = eval
                     best_move = (pawn, (new_row, new_col))
